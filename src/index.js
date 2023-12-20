fbec1bab19d2da46a7ced595ce0f7cc7da60602c
rahbhee <ahmedrabi2003@gmail.com>
2023-12-20 22:35:34 +0100

    changed font family

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+const aboutDisplay = document.querySelector('.about');
+const aboutBtn = document.querySelector('#about');
+const introSection = document.querySelector('.intro');
 
+aboutBtn.addEventListener("click", () => {
+    introSection.style.display = 'none'
+    aboutDisplay.style.display = 'block'
+})
 
